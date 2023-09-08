73e6f00b757338484384830a20fa7558e580b260
Felipe Araujo Camelo <48761994+ViktorReznovCOD@users.noreply.github.com>
2023-09-08 17:58:55 -0300

    Configuração inicial do bot: O código novo mantém a configuração inicial do bot, incluindo o token do Telegram e a conexão com o banco de dados.
    Remoção do comando 'delete_webhook': O comando bot.delete_webhook() foi removido no código novo, pois não era mais necessário para desativar o webhook.
    
    Adição de comandos 'Abrir_chamado' e 'capturaTitulo': Foram adicionados comandos para permitir que os usuários abram chamados e capturem informações como título e descrição.
    
    Processamento de informações do chamado: O código novo processa as informações do chamado, realiza um INSERT no banco de dados e fornece uma resposta ao usuário com os detalhes do chamado aberto.
    
    Melhoria na formatação da resposta do bot: A resposta do bot foi melhorada com formatação adequada e detalhes do chamado aberto.
    
    Aprimoramento na listagem de chamados: A listagem de chamados abertos foi aprimorada no código novo para exibir informações relevantes.
    
    Respostas do bot aprimoradas: As respostas do bot foram aprimoradas com formatação e mensagens mais informativas.
    
    Manutenção da função 'mensagemgeral': A função 'mensagemgeral' que responde a qualquer chamada do usuário foi mantida, mas com melhorias na formatação das mensagens.

--- bot_helpdesk.py
+++ bot_helpdesk.py
@@ -1,53 +1,96 @@
 import telebot, mysql.connector, time
 TokenBot1 = "5831647116:AAHDf0kOEPaEBZk5-W05gQZueSFuyzsdDDA"
 bot = telebot.TeleBot(TokenBot1)
-#bot.delete_webhook()
 mydb = mysql.connector.connect(database='glpi', host='localhost', user='glpi', password='123')
 
-@bot.message_handler(commands=['Iniciar'])
-def iniciar(message):
-    bot.reply_to(message,'''
-        🤖 Descrição do Problema: Por favor, descreva detalhadamente qual é o problema ou a solicitação que você está enfrentando no GLPI:       
-                 ''')
+''' bot.delete_webhook()
+é usada em bots do Telegram para desativar o webhook previamente configurado. Um webhook é uma forma de receber atualizações em tempo real do Telegram sempre que um evento ocorre em uma conversa com o bot, como quando um usuário envia uma mensagem.
+
+Quando um webhook está ativo, o Telegram envia automaticamente as atualizações para o URL especificado no webhook, em vez de você ter que verificar constantemente o servidor do Telegram em busca de novas mensagens. Isso pode ser útil em cenários em que você deseja que seu bot responda instantaneamente às mensagens dos usuários.
+
+No entanto, há situações em que você pode querer desativar o webhook. Por exemplo:
+
+Depuração e teste: Durante o desenvolvimento de um bot, você pode querer desativar o webhook temporariamente para facilitar o teste e a depuração do código do bot sem que as atualizações sejam enviadas automaticamente para o seu servidor.
+
+Mudança de abordagem: Se você estava usando um webhook e decide mudar para um método de polling (verificação periódica das atualizações) para receber mensagens, você deve desativar o webhook.
+
+Manutenção do servidor: Em alguns casos, você pode precisar desativar o webhook temporariamente para manutenção do servidor onde o bot está hospedado.
+
+Para desativar o webhook e voltar a usar o método de polling para receber mensagens, você pode chamar a função bot.delete_webhook().
+
+Exemplo de uso:
+
+python
+Copy code
+
+import telebot
+
+# Inicialize o bot
+bot = telebot.TeleBot("TOKEN_DO_SEU_BOT")
+
+# Desative o webhook
+bot.delete_webhook()
+
+# Resto do seu código para configurar a interação com os usuários usando polling
+bot.infinity_polling()
+Lembre-se de que, após desativar o webhook, você precisará usar um loop de polling, como bot.infinity_polling(), para verificar e responder às mensagens dos usuários.
+
+
+'''
 @bot.message_handler(commands=['Ajuda']) # comando que chama a função abaixo"
 def ajuda(message):
-    print('menu ajuda')
+    print('Menu ajuda!...')
     bot.reply_to(message, f"""ℹ️ Bem-vindo à seção de Ajuda!\n\n
         Aqui estão algumas opções para você:\n
         /Iniciar: Abre um novo chamado.\n
         /Meus_chamados: Lista seus chamados abertos.\n\n
         ℹ️ Para acessar o GLPI, acesse: [GLPI](http://172.16.41.19/glpi)""")
-
+    
+@bot.message_handler(commands=['Abrir_chamado'])
+def AbrirChamado(message):
+    print('Abrir chamados!...')
+    msg = bot.reply_to(message, f'🤖 Para abrir um novo chamado, informe um título:')
+    bot.register_next_step_handler(msg, capturaTitulo)
+def capturaTitulo (message):
+    global titulo 
+    titulo = message.text
+    print('Capturei o titulo: -',titulo)
+    msg = bot.reply_to(message, f'🤖 Conte, como podemos te ajudar?')
+    bot.register_next_step_handler(msg, descricaoChamado)
+def descricaoChamado (message):
+    descricao = message.text
+    print('Capturei a descrição: -',descricao)
+    msg = bot.reply_to(message,f'🤖 Certo!... um momento pfvr....')
+    # PROCESSA INFORMAÇÕES E FAZ O INSERT + COMMIT NO BANCO
+    bot.send_message(message.chat.id, f'''
+                     🤖 Chamado aberto com sucesso
+                     ⏹️ Titulo: {titulo}
+                     ⏹️ Descrição :{descricao}
+                     ''')
 @bot.message_handler(commands=['Meus_chamados'])
 def Meus_chamados(message):
-    #feedback terminal
-    print('Usuário selecionou "Meus_chamados()"')
     bot.send_message(chat_id=message.chat.id,text='um momento por favor...')
-    time.sleep(3)
+    time.sleep(2)
     if mydb.is_connected():
-        cursor = mydb.cursor()
-        # aqui eu tenho que colher quem é o usuário e listar os chamados.
-        query = "SELECT id,name FROM glpi_tickets;"
-        cursor.execute(query)
-        resultado = cursor.fetchall()
-        print(resultado)
-        response = "Seus chamados abertos são:\n"
-        for resultados in resultado:
-            chamado_id = resultado[0]
-            titulo = resultado[0]
-            response += f"- ID: {chamado_id}\n   Título: '{titulo}'\n"
-            bot.reply_to(message,response)
+        cursor = mydb.cursor() 
+        cursor.execute("SELECT id,name FROM glpi_tickets;")
+        resultados = cursor.fetchall()
+        response = f"Os chamados abertos são:\n"
+        for resultado in resultados:
+            i=+1 # contador
+            bot.reply_to(message,f'{resultado[i]}')
+            lista = []
+            lista.append(resultado[:])
 @bot.message_handler(func=lambda message:True) # comando que chama a função abaixo respondendo toda e qualquer chamada.
 def mensagemgeral(message):
     print(f'Nome do usuário que interagiu: ', message.from_user.first_name)
     firstname = message.from_user.first_name #capturando primeiro nome do user no chat
     lastname = message.from_user.last_name
-    name = (f'{firstname}'+f' {lastname}')
-    print(f'ID user:', message.chat.id)
+    nomeuser = (f'{firstname}'+f' {lastname}')
     bot.reply_to(message,f"""
-        🤖 Olá {name}, tudo bem? escolha uma das opções abaixo e toque na desejada:
-        /Ajuda: Opções de ajuda ao usuário.
-        /Iniciar: Abrir um novo chamado.
-        /Meus_chamados: Listar chamados abertos
-                 """)    
+        Olá {nomeuser}, tudo bem? escolha uma das opções abaixo e toque na desejada:
+        [⁉️] /Ajuda: Opções de ajuda ao usuário.
+        [➕] /Abrir_chamado: Para abrir um novo chamado
+        [👀] /Meus_chamados: Listar chamados abertos
+        """)    
 bot.infinity_polling()
--- main.py
+++ main.py
@@ -1,38 +1,19 @@
-import telebot
-import mysql.connector
-
+import telebot, mysql.connector, time
+from telebot import types
 TokenBot1 = "5831647116:AAHDf0kOEPaEBZk5-W05gQZueSFuyzsdDDA"
-TokenBot2 = "5831647116:AAHDf0kOEPaEBZk5-W05gQZueSFuyzsdDDA"
 bot = telebot.TeleBot(TokenBot1)
-bot2 = telebot.TeleBot(TokenBot2)
-bot.delete_webhook()
 mydb = mysql.connector.connect(database='glpi', host='localhost', user='glpi', password='123')
-'''
-if mydb.is_connected():
-    @bot.message_handler(commands=['inciar', 'ajuda'])
-    def send_welcome(message):
-        bot.reply_to(message, "Howdy, how are you doing?")
 
-    @bot.message_handler(func=lambda message: True)
-    def echo_all(message):
-        bot.reply_to(message, message.text)
+@bot.message_handler(commands=['start'])
+def register(message):
+    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    reg_button = types.KeyboardButton(text="Share your phone number", request_contact=True)
+    keyboard.add(reg_button)
+    response = bot.send_message(message.chat.id, 
+                                "You should share your phone number", 
+                                reply_markup=keyboard)
+    print(response.contact)  # response.contact = None here
 
-    bot.infinity_polling()
-else:
-    print("Não conectou...")
-'''
-@bot.message_handler(commands=['iniciar'])
-def iniciar(message):
-    bot.reply_to(message,"Deseja abrir um chamado?")
-@bot.message_handler(commands=['ajuda']) # comando que chama a função abaixo"
-def ajuda(message):
-    print('menu ajuda')
-    bot.reply_to(message, "Tudo sobre ajuda.................")
-@bot.message_handler(func=lambda message:True) # comando que chama a função abaixo respondendo toda e qualquer chamada.
-def mensagemgeral(message):
-    bot.reply_to(message, """
-        Mensagem padrão
-        /ajuda: Opções de ajuda ao usuário.
-        /iniciar: Abrir um novo chamado.        
-""")
-bot.infinity_polling()
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
+

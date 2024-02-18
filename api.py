8aabd16021efd2e36e6873d7e710cf8da7d89ae2
F3lipe Araujo <ofc.felipearaujo@gmail.com>
2024-02-18 20:47:00 -0300

    Adicionado o arquivo bot_telegram.py para implementar um bot do Telegram. Criada a funcao adicionar_chamado(dados_chamado) em sourcer.py para adicionar chamados ao sistema. Atualizada a classe Bot em bot_telegram.py para interagir com a funcao adicionar_chamado. Corrigidos pequenos erros e ajustes de formatacao nos arquivos.

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import time
 class Api:
     def __init__(self,user_token):
         self.user_token = user_token
-        self.urlv1 = "http://localhost/glpi/glpi/apirest.php/"
+        self.urlv1 = "http://localhost/glpi/glpi/apirest.php/" #exemplo
         self.max_retries = 10
         self.seconds_time_between_retries = 3
         self.headers = {
@@ -15,25 +15,36 @@ class Api:
         finalurl = self.urlv1 + endpoint
         
         if additional_attributes:
+            print("--debug api.py: Com additional atrubutes")
             self.headers["Session-Token"]=additional_attributes
         else:
+            print("--debug api.py: Sem additional atributes")
             pass
         
         try:
             if method == "POST":
+                print("--debug api.py: POST")
                 response = requests.post(finalurl,headers=self.headers,json=body)
             else:
+                print("--debug api.py: GET")
                 response = requests.get(finalurl, headers=self.headers)
-        
-            if response.status_code==400:
+
+            # 207 (Multi-Status) with id of added items and errors.
+
+            if response.status_code==400: # (Bad-Request)
                 print(response.url)
                 print(response.text)
                 time.sleep(10)
                 raise Exception("response.status_code == 400")
-            elif response.status_code==401:
+            elif response.status_code==401: # (UNAUTHORIZED)
                 print(response.url)
                 print(response.text)
                 raise Exception("response.status_code == 401")
+            elif response.status_code==404:
+                print(response.url)
+                print(response.text)
+                raise Exception("response.status_code == 404")
+            
             return response.json()
         
         except Exception as err:
--- soucer.py
+++ soucer.py
@@ -1,76 +1,40 @@
-from Projetos.PyGLPIBot.api import Api
+from api import Api
 import auth,pprint
-from faker import Faker
+from faker import Faker # usado na criação de dados sintéticos
 
-dados_auth=auth.credentials("basic")
-api = Api(dados_auth)
+dados_auth=auth.credentials("basic") # dados de autenticação nos sistemas (tokens, usuarios, senhas e etc.)
+api = Api(dados_auth) 
 session_token = Api.session_token(api)
 
-
-# PROFILE
-def get_my_profiles(api):
-    # a resposta é um json com chave "myprofiles", valor é um array de jsons...
-    response = api.call("getMyProfiles", additional_attributes=session_token)
-    if "myprofiles" in response:
-        my_profiles = response["myprofiles"]
-        list_profiles = [profile["name"] for profile in my_profiles if "name" in profile]
-        return list_profiles
-    else:
-        print("Erro ao obter perfis.")
-        return []
-
-def get_active_profile(api):
-    pass
-
-def change_active_profile(api):
-    pass
-
-# ENTITIES
-def get_my_entities(api):
-    pass
-def get_active_entities(api):
-    pass
-def change_active_entities(api):
-    pass
-
-# ITEMS
-def get_an_item(api,item_type):
-    endpoint = item_type
-    response = api.call(endpoint,additional_attributes=session_token)
-    return response
-def get_all_items(api):
-    pass
-def get_sub_items(api):
-    pass
-def get_multiple_items(api):
-    pass
-def add_items(api,item_type):
-    fake = Faker()
-    endpoint = item_type
-    body = {
-        "input":{"name":"computador do(a) {}".format(fake.name()),
-                 "serial":"{}".format(fake.uuid4()),
-                 "otherserial":"{}".format(fake.uuid4()),
-                 "contact":"{}".format(fake.name()),
-                 "contact_num":"{}".format(fake.phone_number()),
-                 #"users_id_tech":"{}".format(random.choice()),# "id_tecnico", o padrão é o "0"
-                 #"groups_id_tech":"{}".format(random.choice()),# "id_grupo_tecnico, o padrão é o "0"
-                 "comment":"{}".format(fake.text()),
-                #  "users_id":"{}".format(random.choice()),#"id do usuário, o padrão é o "0"
-                #  "groups_id":"{}".format(random.choice()),# id do grupo, o padrão é o "0"
-                #  "status"
+def adicionar_chamado(dados_chamado): # OK
+    deu_certo = False
+    try:
+            
+        _api = api
+        endpoint = "Ticket"
+        body = {
+            "input": {
+                "name":dados_chamado["name"],
+                "content":dados_chamado["content"],
+                "comment":dados_chamado["comment"],
+                "users_id_recipient":374
+            }
         }
-    }
-    response = api.call(endpoint,method="POST",additional_attributes=session_token,body=body)
-    return response
-    
-
-
+        response = api.call(endpoint,"POST",additional_attributes=session_token,body=body)
+        if api.call.status_code == 200:
+            print(response)
+            deu_certo = True
+            return deu_certo
+        else:
+            print("--debug DEU CERTO É {}".format(deu_certo))
+            return deu_certo
+    except Exception as e:
+        print("--debug\n falha na função -> adicionar_chamado(dados_chamado)\nerror: {}".format(e))
+        
 def _main():
     # a=add_items(api,"Computer")
     # print(a)
-    itens = get_an_item(api,"Computer")
-    pprint.pprint(itens)
-    
+    # itens = get_an_item(api,"Computer")
+    pass
 if __name__ == "__main__":
     _main()
